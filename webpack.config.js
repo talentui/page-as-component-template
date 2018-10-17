0cccb6a08caa360efc9e79f3ad946d15f0a6f636
lihe3 <lihe3@corp.beisencorp.com>
2018-10-17 17:57:43 +0800

    chore(webpack配置): 完善webpack配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
-const path = require('path')
-const merge = require('webpack-merge');
-const Analyzer = require('webpack-bundle-analyzer');
-
-
-const webpackConfig =  require("@talentui/webpack-config")({
+const path = require("path");
+const merge = require("webpack-merge");
+const Analyzer = require("webpack-bundle-analyzer");
+const { appId, componentCode, name, version } = require("./package.json");
+const library = name.replace(/@|\-|\//g, "_");
+const buildProd =
+    require("@talentui/webpack-config/src/helpers/parse-mode")() ===
+    "production";
+const webpackConfig = require("@talentui/webpack-config")({
     entry: {
-        main: './src/index'
+        main: "@talentui/thunder-component-bootstrap/dynamic"
     },
     port: 3001,
     extractStyles: false,
@@ -13,13 +16,26 @@ const webpackConfig =  require("@talentui/webpack-config")({
     //     "react": path.resolve(__dirname, 'node_modules/react')
     // }
     define: {
-        __HEHE: false
+        "process.env": {
+            library: JSON.stringify(library),
+            packageName: JSON.stringify(name),
+            appId: JSON.stringify(appId),
+            componentCode: JSON.stringify(componentCode),
+            componentEntry: JSON.stringify(path.resolve(__dirname, "src/index"))
+        }
     },
-    projectType: 'module'
+    projectType: "module"
 });
 
 module.exports = merge(webpackConfig, {
-    plugins: [
-        new Analyzer.BundleAnalyzerPlugin({})
-    ]
+    plugins: buildProd
+        ? [
+              new Analyzer.BundleAnalyzerPlugin({
+                  analyzerMode: "static",
+                  generateStatsFile: true,
+                  reportFilename: `report-${version}.html`,
+                  statsFilename: `stats-${version}.json`
+              })
+          ]
+        : []
 });
