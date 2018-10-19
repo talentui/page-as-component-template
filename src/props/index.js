5b53abd677a6bef97dafe765b26ebeb8e5631bca
lihe3 <lihe3@corp.beisencorp.com>
2018-10-19 10:08:00 +0800

    build(属性配置): 调整文件结构，去除loader

---
+++ src/props/index.js
@@ -0,0 +1,12 @@
+/** 导出属性配置 
+ * Demo Code:
+import Input from './input'
+export default [
+    {
+        pType: Input,
+        name: "编辑内容",
+        key: "value",
+        defaultValue: "123"
+    }
+]
+*/
